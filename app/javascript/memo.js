052ee1cdec2ea9f9cad0f070dc389344bd6a29b4
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-10-30 19:36:22 +0900

    メモ機能実装

---
+++ app/javascript/memo.js
@@ -0,0 +1,10 @@
+function memo() {
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+  　　const formData = new FormData(document.getElementById("form"));
+  　　const XHR = new XMLHttpRequest();
+  　　XHR.open("POST", "/posts", true);
+  　　XHR.responseType = "json";
+　});
+}　
+window.addEventListener("load", memo);

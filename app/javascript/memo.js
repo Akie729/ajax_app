06863c8e22db8e4c8701a6b004ebaeb6d02a8efb
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-11-01 12:15:50 +0900

    gem rails_12factorの追加

---
+++ app/javascript/memo.js
@@ -0,0 +1,13 @@
+<<<<<<< Updated upstream
+=======
+function memo() {
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.getElementById("form"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+  });
+}
+window.addEventListener("load", memo);
+>>>>>>> Stashed changes
